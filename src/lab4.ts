912498b544ae4f96963dd9de1730fc1cc2b8f4fc
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-10-30 16:50:11 +0200

    Add Clear and Simulation Step buttons.

--- src/lab4.ts
+++ src/lab4.ts
@@ -9,25 +9,38 @@ declare global {
 document.addEventListener("DOMContentLoaded", () => {
     const gridInput = document.body.querySelector("#grid-input") as HTMLButtonElement
     const applyGridButton = document.body.querySelector("#apply-grid-button") as HTMLButtonElement
+    const clearGridButton = document.body.querySelector("#clear-grid-button") as HTMLButtonElement
+    const simulationStepButton = document.body.querySelector("#simulation-step-button") as HTMLButtonElement
     const simulationRunButton = document.body.querySelector("#simulation-run-button") as HTMLButtonElement
 
     const getGrid = () => Number(gridInput.value)
 
-    window.game = new GameOfLife(5)
+    window.game = new GameOfLife(10)
     document.body.append(window.game.wrapper)
 
 
     applyGridButton.addEventListener("mousedown", () => {
         console.log(`New grid is: ${getGrid()}`)
-
         window.game.wrapper.remove()
         window.game = new GameOfLife(getGrid(), getGrid())
-        document.body.append(window.sim.wrapper)
+        document.body.append(window.game.wrapper)
+    })
+
+    clearGridButton.addEventListener("mousedown", () => {
+        console.log(`Clear grid`)
+        window.game.tiles.forEach(row => row.forEach(cell => {cell.isActive = false; cell.div.innerText = ""}))
+    })
+
+    simulationStepButton.addEventListener("mousedown", () => {
+        window.game.simulationStep()
     })
 
     simulationRunButton.addEventListener("mousedown", () => {
-        if(window.game.isRunning)
-        window.game.runSimulation()
+        if (!window.game.isRunning)
+            window.game.runSimulation()
+        else
+            window.game.stopSimulation()
+        simulationRunButton.innerText = window.game.isRunning ? "Stop simulation" : "Run simulation"
     })
 
 
