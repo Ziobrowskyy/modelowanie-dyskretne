d3ab359d363d3a487d774289a0556c88e8942ed1
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-10-30 16:52:49 +0200

    Make Game of Life working.

--- src/cellular_automata/gameOfLife.ts
+++ src/cellular_automata/gameOfLife.ts
@@ -1,8 +1,10 @@
 import CellularAutomata from "./cellularAutomata.js";
+import Utils from "../utils.js";
 
 export default class GameOfLife extends CellularAutomata {
     isRunning: boolean = false
     #simulationInterval?: number
+
     constructor(width: number = 50, height: number = width) {
         super(width, height)
         this.tiles.forEach(row => row.forEach(tile => {
@@ -11,7 +13,7 @@ export default class GameOfLife extends CellularAutomata {
     }
 
     runSimulation() {
-        if(this.isRunning)
+        if (this.isRunning)
             return
         this.isRunning = true
         this.#simulationInterval = setInterval(() => {
@@ -20,15 +22,32 @@ export default class GameOfLife extends CellularAutomata {
     }
 
     stopSimulation() {
-        if(!this.isRunning)
+        if (!this.isRunning)
             return
         this.isRunning = false
         clearInterval(this.#simulationInterval)
     }
 
+    getNeighbours(x: number, y: number, tiles: boolean[][]) {
+        let sum = 0
+        for (let dy = -1; dy <= 1; dy++) {
+            for (let dx = -1; dx <= 1; dx++) {
+                if (dy === 0 && dx === 0)
+                    continue
+                const wy = Utils.wrapValue(y + dy, 0, this.height)
+                const wx = Utils.wrapValue(x + dx, 0, this.width)
+                sum += Number(tiles[wy][wx])
+            }
+        }
+        return sum
+    }
+
     simulationStep() {
-        this.tiles.forEach(row => row.forEach(tile => {
-            tile.isActive = Math.random() > 0.5
-        }))
+        const oldTiles = this.tiles.map(row => row.map(tile => tile.isActive))
+        oldTiles.forEach((row, y) =>
+            row.forEach((wasAlive, x) => {
+                const n = this.getNeighbours(x, y, oldTiles)
+                this.tiles[y][x].isActive = n === 2 && wasAlive || n === 3
+            }))
     }
 }
