11b1691f273fcf2ec5264c43500fa05dbbfd3a4f
ziobrowskyy <thomas.ziobrowski@gmail.com>
2021-11-17 10:02:05 +0100

    Tidy up html file and lab5.js

--- src/lab5.ts
+++ src/lab5.ts
@@ -3,16 +3,13 @@ import ForestFire from "./cellular_automata/ForestFire.js";
 let forestFire: ForestFire
 
 document.addEventListener("DOMContentLoaded", () => {
-    const gridInput = document.body.querySelector("#grid-input") as HTMLInputElement
-    const applyGridButton = document.body.querySelector("#apply-grid-button") as HTMLButtonElement
     const simulationStepDelayInput = document.body.querySelector("#simulation-step-delay-input") as HTMLInputElement
     const simulationStepButton = document.body.querySelector("#simulation-step-button") as HTMLButtonElement
     const simulationRunButton = document.body.querySelector("#simulation-run-button") as HTMLButtonElement
 
-    const getGrid = () => Number(gridInput.value)
     const getSimulationStepDelay = () => Number(simulationStepDelayInput.value)
 
-    forestFire = new ForestFire(getGrid(), getGrid())
+    forestFire = new ForestFire()
     document.body.append(forestFire.wrapper)
 
     const setSimulation = (value: boolean) => {
@@ -23,18 +20,11 @@ document.addEventListener("DOMContentLoaded", () => {
         simulationRunButton.innerText = value ? "Stop simulation" : "Run simulation"
     }
 
-    applyGridButton.addEventListener("mousedown", () => {
-        setSimulation(false)
-        forestFire.wrapper.remove()
-        forestFire = new ForestFire(getGrid(), getGrid())
-        document.body.append(forestFire.wrapper)
+    simulationStepDelayInput.addEventListener("input", () => {
+        const newDelay = getSimulationStepDelay()
+        console.log(`Simulation step delay ${newDelay}`)
+        forestFire.simulationDelay = newDelay
     })
-
-    // simulationStepDelayInput.addEventListener("input", () => {
-    //     const newDelay = getSimulationStepDelay()
-    //     console.log(`Simulation step delay ${newDelay}`)
-    //     window.gameDiv.simulationStepDelay = newDelay
-    // })
 
     simulationStepButton.addEventListener("mousedown", () => {
         console.log(`Simulation step`)
